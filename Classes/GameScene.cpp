3e45f53c5357d1fb0f4ca4cdf7c8b11b23824a18
NinePinNoTap <1100484@live.abertay.ac.uk>
2016-07-27 00:18:36 +0100

    Finished Tutorial 11 - Fly Faby Fly

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -10,6 +10,7 @@ Scene* GameScene::createScene()
     // 'scene' is an autorelease object
     auto scene = Scene::createWithPhysics();
 	scene->getPhysicsWorld()->setDebugDrawMask(PhysicsWorld::DEBUGDRAW_ALL);
+	scene->getPhysicsWorld()->setGravity(Vec2(0, 0));
     
     // 'layer' is an autorelease object
     auto layer = GameScene::create();
@@ -40,7 +41,7 @@ bool GameScene::init()
 
 	auto edgeBody = PhysicsBody::createEdgeBox(visibleSize, PHYSICSBODY_MATERIAL_DEFAULT, 3);
 	edgeBody->setCollisionBitmask(OBSTACLE_COLLISION_BITMASK);
-	edgeBody->setContactTestBitmask(false);
+	edgeBody->setContactTestBitmask(true);
 
 	auto edgeNode = Node::create();
 	edgeNode->setPosition(centerScreen);
@@ -55,6 +56,12 @@ bool GameScene::init()
 	contactListener->onContactBegin = CC_CALLBACK_1(GameScene::onContactBegin, this);
 	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(contactListener, this);
 
+	auto touchListener = EventListenerTouchOneByOne::create();
+	touchListener->setSwallowTouches(true);
+	touchListener->onTouchBegan = CC_CALLBACK_2(GameScene::onTouchBegan, this);
+	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(touchListener, this);
+
+	this->scheduleUpdate();
 
     return true;
 }
@@ -78,4 +85,22 @@ bool GameScene::onContactBegin(cocos2d::PhysicsContact &contact)
 	}
 
 	return true;
+}
+
+bool GameScene::onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event)
+{
+	bird->Fly();
+	this->schedule(schedule_selector(GameScene::StopFlying), BIRD_FLY_DURATION);
+
+	return true;
+}
+
+void GameScene::StopFlying(float dt)
+{
+	bird->StopFlying();
+}
+
+void GameScene::update(float dt)
+{
+	bird->Fall();
 }
